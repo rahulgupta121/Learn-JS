27b5001ec862f5f41897e05c1511bd529d6dd0bd
rahul kumar <rahul.kumar@ihub-data.iiit.ac.in>
2024-05-13 22:36:47 +0530

    object added

---
+++ object desctruturing.js
@@ -0,0 +1,11 @@
+// Basic object destructuring
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30,
+    city: 'New York'
+  };
+  
+  const { firstName, lastName, age } = person;
+
+  console.log(person)
